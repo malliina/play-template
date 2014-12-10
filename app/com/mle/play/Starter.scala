8868621b24574c028ad111b8bb0920fa7b6c0034
malliina <malliina123@gmail.com>
2014-12-10 23:04:05 +0200

    cleanup

--- app/com/mle/play/Starter.scala
+++ app/com/mle/play/Starter.scala
@@ -1,8 +1,20 @@
 package com.mle.play
 
+import java.util.concurrent.TimeUnit
+
+import scala.concurrent.{ExecutionContext, Future}
+
 /**
  * @author mle
  */
 object Starter extends PlayLifeCycle {
   override def appName: String = "p"
+
+  def stop() = {
+    nettyServer foreach (server => {
+      server.allChannels.close().await(2, TimeUnit.SECONDS)
+      server.stop()
+    })
+    Future(System.exit(0))(ExecutionContext.Implicits.global)
+  }
 }
