e691f978c74142b3bc13eeabf2f810d58f730045
malliina <malliina123@gmail.com>
2015-02-01 15:28:55 +0200

    cleaned up

--- project/PlayBuild.scala
+++ project/PlayBuild.scala
@@ -1,16 +1,4 @@
-import com.mle.file.StorageFile
-import com.mle.sbt.GenericKeys._
-import com.mle.sbt.GenericPlugin
-import com.mle.sbt.azure.AzurePlugin
-import com.mle.sbt.mac.MacKeys._
-import com.mle.sbt.mac.MacPlugin
-import com.mle.sbt.mac.MacPlugin._
-import com.mle.sbt.unix.LinuxPlugin
-import com.mle.sbt.win.{WinKeys, WinPlugin}
 import com.mle.sbtplay.PlayProjects
-import com.typesafe.sbt.SbtNativePackager
-import com.typesafe.sbt.SbtNativePackager._
-import com.typesafe.sbt.packager.linux
 import sbt.Keys._
 import sbt._
 
@@ -20,39 +8,28 @@ object PlayBuild extends Build {
   lazy val commonSettings = Seq(
     organization := "com.github.malliina",
     version := "0.2.1",
-    scalaVersion := "2.11.4",
+    scalaVersion := "2.11.5",
     retrieveManaged := false,
     fork in Test := true,
     updateOptions := updateOptions.value.withCachedResolution(true),
     libraryDependencies ++= Seq(
-      "com.github.malliina" %% "play-base" % "0.1.2",
-      "com.scalatags" %% "scalatags" % "0.4.2")
-  ) ++ nativePackagingSettings
-
-  lazy val nativePackagingSettings = SbtNativePackager.packagerSettings ++
-    WinPlugin.windowsSettings ++
-    LinuxPlugin.rpmSettings ++
-    LinuxPlugin.debianSettings ++
-    GenericPlugin.confSettings ++
-    AzurePlugin.azureSettings ++ Seq(
-    WinKeys.upgradeGuid in Windows := "",
+      "com.github.malliina" %% "play-base" % "0.2.2"),
     mainClass := Some("com.mle.play.Starter"),
-    linux.Keys.maintainer := "Michael Skogberg <malliina123@gmail.com>",
-    // why conf?
-    linux.Keys.packageSummary in Linux := "MusicPimp summary here.",
-    manufacturer := "Skogberg Labs",
     exportJars := true,
     javacOptions ++= Seq("-source", "1.7", "-target", "1.7"),
-    scalacOptions += "-target:jvm-1.7"
-  ) ++ macConfig
-
-  def macConfig = MacPlugin.macSettings ++ myMacConfSettings ++ Seq(
-    jvmOptions ++= Seq("-Dhttp.port=8456"),
-    launchdConf := Some(defaultLaunchd.value)
+    scalacOptions ++= Seq(
+      "-target:jvm-1.7",
+      "-deprecation",
+      "-encoding", "UTF-8",
+      "-unchecked",
+      "-feature",
+      "-language:existentials",
+      "-language:higherKinds",
+      "-language:implicitConversions",
+      "-Xfatal-warnings",
+      "-Xlint",
+      "-Yno-adapted-args",
+      "-Ywarn-dead-code",
+      "-Ywarn-numeric-widen")
   )
-
-  def myMacConfSettings = MacPlugin.macSettings ++ inConfig(Mac)(Seq(
-    appIcon := Some(pkgHome.value / "guitar.icns"),
-    displayName := "P"
-  ))
 }
