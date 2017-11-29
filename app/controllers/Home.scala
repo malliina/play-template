b04ef273ae82b47cba44b30ecf9139b05911f64a
Michael Skogberg <michael.skogberg@futurice.com>
2017-11-29 20:09:50 +0200

    Up deps

--- app/controllers/Home.scala
+++ app/controllers/Home.scala
@@ -3,7 +3,8 @@ package controllers
 import controllers.Assets.Asset
 import play.api.mvc._
 
-class Home(comps: ControllerComponents, assets: Assets) extends AbstractController(comps) {
+class Home(comps: ControllerComponents, assets: AssetsBuilder)
+  extends AbstractController(comps) {
   def index = Action(Ok(AppTags.index("Hoi!")))
 
   def versioned(path: String, file: Asset): Action[AnyContent] =
